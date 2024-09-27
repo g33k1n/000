0c1e800da1e9212ec1721bb48afcd15c62e74604
RAZEMANn2 <babykinartem36@gmail.com>
2024-09-27 12:16:14 +0300

    Update fds.py

--- fds.py
+++ fds.py
@@ -1,2 +1,6 @@
-print("HI")
-print("КАК ДЕЛА")
+#Исправлена формула вычисления корней через дискриминант
+if discriminant > 0:
+        # Два различных корня
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return x1, x2
